5c8d82910b1b589b97bdb5dbeb937e2b95ddeba4
jengerred <jengerred@aol.com>
2026-04-07 21:28:15 -0400

    changed to 8.0 .net

--- obj/Debug/net9.0/BakeryBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/BakeryBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakeryBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72aa0c754c1edd78788f3ba73b41a4a53039dd87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd913fa8a243fbc0f2ebe1846cb8f076946df8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakeryBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakeryBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
